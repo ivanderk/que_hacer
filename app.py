11c716ac6f42d13f26796f9cb40ff02dbd1ff1e9
Iwan van der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2023-02-13 21:40:00 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,44 @@
+# project/app.py
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+
+app.app_context().push()
+db.create_all()
+
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = Todo(text=request.form['todo_text'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<id>')
+def complete(id):
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
