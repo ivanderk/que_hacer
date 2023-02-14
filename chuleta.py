43a6b6977498eb9505719cd4a99964f362b6d970
Iwan van der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2023-02-14 22:21:17 +0100

    Person.show_me fix for windows

--- chuleta.py
+++ chuleta.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import webbrowser
+import time
 
 def get_person(nombre, apellidos, edad):
     return {"nombre": nombre, "apellidos":apellidos, "edad": edad}
@@ -23,12 +24,14 @@ class Person:
         return f"Person: {self.nombre} {self.apellidos}"
     
     def show_me(self):
-        webbrowser.open_new(f"https://www.google.com/search?q={self.nombre}+{self.apellidos}")
+        webbrowser.open_new_tab(f"https://www.google.com/search?q={self.nombre}+{self.apellidos}")
 
 person2 = Person("Patricio", "Estrella Del Mar", 4)
 print(f"Person: {person2.nombre} {person2.apellidos}")
 print(person2.identify())
 person2.show_me()
 
+#On Windows we need to wait a bit to have the action completed
+time.sleep(1)
 
 
