ec7895fcf2843595b367ae720f7c129c32b39c6a
Iwan van der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2023-02-21 20:12:06 +0100

    Ejemplo modulo

---
+++ deberes.py
@@ -0,0 +1,16 @@
+
+def generar_saludo(persona):
+
+    if persona["genero"] == 0:
+        return f"Hola Señor {persona['nombre']}"
+    else:
+         return f"Hola Señora {persona['nombre']}"
+
+if __name__ == "__main__":
+    datos_persona = {"nombre": "Paquito", "genero": 0}
+    saludo = generar_saludo(datos_persona)
+    print(saludo)
+
+    datos_persona = {"nombre": "Paquita", "genero": 1}
+    saludo = generar_saludo(datos_persona)
+    print(saludo)
---
+++ functionalidad.py
@@ -0,0 +1,6 @@
+
+import deberes
+
+datos_persona = {"nombre": "Paquito", "genero": 0}
+saludo = deberes.generar_saludo(datos_persona)
+print(saludo)
