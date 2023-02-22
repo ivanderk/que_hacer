c049a6d0063961895261636e03b2a535481f23d7
Iwan van der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2023-02-22 08:03:30 +0100

    Reorderd folder

--- vcal/vcal-usage.py
+++ vcal/vcal-usage.py
@@ -4,8 +4,12 @@ import vcal_artesanal
 
 #import vcal_battery 
 
+# beginevent = datetime.strptime("28/03/2023 09:00",  '%d/%m/%Y %H:%M')
+# endevent = datetime.strptime("28/03/2023 13:50",  '%d/%m/%Y %H:%M')
+
 beginevent = datetime.strptime("28/03/2023 09:00",  '%d/%m/%Y %H:%M').astimezone(tz=timezone('CET'))
 endevent = datetime.strptime("28/03/2023 13:50",  '%d/%m/%Y %H:%M').astimezone(tz=timezone('CET'))
+
 ical1 = vcal_artesanal.get_ical_text("Example event", "John Doe is attending Example Event", beginevent, endevent)
 print(ical1)
 
